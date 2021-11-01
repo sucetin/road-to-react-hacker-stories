f3d81aae08ca10347f05f7469cfa7316d7d5ac82
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-11-01 06:00:14 -0700

    working with asyncrhonous data

--- src/App.js
+++ src/App.js
@@ -12,28 +12,56 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 }
 
-const App = () => {
-  console.log("App renders");
-  const stories = [
-    {
-      title: 'React',
+const initialStories = [
+  {
+    title: 'React',
       url: 'https://reactjs.org/',
       author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
+const App = () => {
+  console.log("App renders");
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    getAsyncStories().then((result) => {
+      setStories(result.data.stories);
+      setIsLoading(false);
+    })
+    .catch(setIsError(true));
+  }, []);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories)
+  }
 
 const handleSearch = (event) => {
   setSearchTerm(event.target.value);
@@ -45,43 +73,67 @@ const searchedStories = stories.filter((story) => story.title.toLowerCase().incl
     <>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel id='search' label='Search' value={searchTerm} onInputChange={handleSearch}/>
+      <InputWithLabel id='search' value={searchTerm} onInputChange={handleSearch} isFocused>
+        <strong>Search: </strong>
+      </InputWithLabel>  
 
       <hr />
+      {isError && <p>Something went wrong..</p>}
 
-      <List list={searchedStories}/>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      )};
+
+      
       
     </>
     );
 }
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
     ))}
   </ul>
 );
  
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-);
+const Item = ({ item, onRemoveItem }) => {
+  return(
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type='button' onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </li>
+  );
+};
 
-const InputWithLabel = ({id, label, value, type='text', onInputChange}) => {
+const InputWithLabel = ({id, value, type='text', onInputChange, children, isFocused,}) => {
   console.log("Search renders");
+
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if(isFocused && inputRef.current){
+      inputRef.current.focus()
+    }
+  }, [isFocused]);
 
   return(
     <>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
-      <input id={id} type={type} value={value} onChange={onInputChange}/>
+      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} autoFocus={isFocused}/>
     </>
     
 );
